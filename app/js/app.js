3eeed059811159e469058b8e8e66f4cef819900a
Aleksej Chuiko <alex.chuiko13@gmail.com>
2020-05-29 17:09:30 +0300

    SHOPNO ready

---
+++ app/js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	let modal = document.getElementsByClassName("modal");
+	let imgs = document.getElementsByClassName("img");
+	let modalImg = document.getElementsByClassName("modal-content");
+	let caption = document.getElementsByClassName('caption');
+
+	for (let i = 0; i < imgs.length; i++) {
+		imgs[i].onclick = function() {
+			modal[i].style.display = "block";
+			modalImg[i].src = this.src;
+			caption[i].innerHTML = this.alt;
+		}
+	}
+
+	let span = document.getElementsByClassName('close');
+
+	for (let i = 0; i < span.length; i++) {
+		span[i].onclick = function() {
+			modal[i].style.display = "none";
+		}
+	}
+	
+});
